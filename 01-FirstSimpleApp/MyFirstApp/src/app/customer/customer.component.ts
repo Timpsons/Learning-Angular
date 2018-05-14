5d0815792b740d20e1b31114b7785575577731c3
Timpsons <development@goedel.online>
2018-05-14 22:12:47 +0200

    Add new Customer component with 2-way-binding

---
+++ 01-FirstSimpleApp/MyFirstApp/src/app/customer/customer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../domain/customer'
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customer: Customer = {
+      id: 666,
+      name: 'Mr. Winterbottom'
+  }
+
+  constructor() { }
+
+  ngOnInit() { }
+}
